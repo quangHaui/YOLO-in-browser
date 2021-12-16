867f167dfe49fe7c97bc032353cbe12809ba5d1a
quanghaui <2018603149@dhcnhn.edu.vn>
2021-12-16 23:07:41 +0700

    Updates

---
+++ precache-manifest.d8dc15a3fc6c968ea3a0c53c10d8ea5e.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "769e092e85f0411e60a4",
+    "url": "./static/css/main.4623a387.chunk.css"
+  },
+  {
+    "revision": "769e092e85f0411e60a4",
+    "url": "./static/js/main.769e092e.chunk.js"
+  },
+  {
+    "revision": "ce0822b43651057c1f6b",
+    "url": "./static/js/1.ce0822b4.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0e576561204d6db02b0007d2df755482",
+    "url": "./static/media/video.0e576561.mp4"
+  },
+  {
+    "revision": "7896b4ea2db2d76cfd521b9bd72fcbcc",
+    "url": "./static/media/End of speed limit (80kmph).7896b4ea.png"
+  },
+  {
+    "revision": "63a2c7e0e13dc6546735ee1f32c83b95",
+    "url": "./static/media/Speed limit (100kmph).63a2c7e0.png"
+  },
+  {
+    "revision": "aad3f8c69d741830bed13f95b5fdebc1",
+    "url": "./static/media/Speed limit (120kmph).aad3f8c6.png"
+  },
+  {
+    "revision": "6a954cb8049cffb93192592c8efc8a36",
+    "url": "./static/media/Speed limit (30kmph).6a954cb8.png"
+  },
+  {
+    "revision": "382eace719ba27589040540510e0a298",
+    "url": "./static/media/Speed limit (50kmph).382eace7.png"
+  },
+  {
+    "revision": "9134686e677c64296ce954d3251fa923",
+    "url": "./static/media/Speed limit (60kmph).9134686e.png"
+  },
+  {
+    "revision": "f282239251d068e6116fb75e3f06f459",
+    "url": "./static/media/Speed limit (80kmph).f2822392.png"
+  },
+  {
+    "revision": "f278b97ca49f403885aad870a99b7e47",
+    "url": "./static/media/00238.f278b97c.jpg"
+  },
+  {
+    "revision": "126fe6f8867e3757654e51b2eb6c305f",
+    "url": "./static/media/00239.126fe6f8.jpg"
+  },
+  {
+    "revision": "6acc11bd9c73799d813d85997a45e017",
+    "url": "./static/media/00240.6acc11bd.jpg"
+  },
+  {
+    "revision": "f450efc6eaff5c4978b1ffafd65bafd9",
+    "url": "./static/media/00241.f450efc6.jpg"
+  },
+  {
+    "revision": "8494d8b6670066310d1e4ec86e4c353d",
+    "url": "./static/media/00242.8494d8b6.jpg"
+  },
+  {
+    "revision": "6dbaa0014ac8bff91351bcd62ec2cf27",
+    "url": "./static/media/00243.6dbaa001.jpg"
+  },
+  {
+    "revision": "484d5666089004c4592b4ca05e95cdf4",
+    "url": "./static/media/00244.484d5666.jpg"
+  },
+  {
+    "revision": "74215e4e1b4300257e19bc22457e3b9e",
+    "url": "./static/media/00245.74215e4e.jpg"
+  },
+  {
+    "revision": "0f4e79a9568523bfdaf82c747260046f",
+    "url": "./static/media/00246.0f4e79a9.jpg"
+  },
+  {
+    "revision": "e213b2b0581fe4ce4bdb269050270f55",
+    "url": "./static/media/00247.e213b2b0.jpg"
+  },
+  {
+    "revision": "e92888d39b4dd72e01171b2b84b4a6f1",
+    "url": "./static/media/00248.e92888d3.jpg"
+  },
+  {
+    "revision": "06a2db0cce7aad1a73189d98958a1407",
+    "url": "./static/media/00249.06a2db0c.jpg"
+  },
+  {
+    "revision": "f70a565fafde17e484c4cf44ca4dd545",
+    "url": "./static/media/00250.f70a565f.jpg"
+  },
+  {
+    "revision": "636a17c75ae2cc39c6373bec7a189696",
+    "url": "./static/media/00251.636a17c7.jpg"
+  },
+  {
+    "revision": "a4ed7c09300459fbcb198822ab3029ed",
+    "url": "./static/media/00252.a4ed7c09.jpg"
+  },
+  {
+    "revision": "8d957e9f15626301e06a08e2b0e1d23c",
+    "url": "./static/media/00253.8d957e9f.jpg"
+  },
+  {
+    "revision": "74bddc0abd8538364330ad1bf9302c78",
+    "url": "./static/media/00254.74bddc0a.jpg"
+  },
+  {
+    "revision": "687792a4ad7079d636b5264384ec4710",
+    "url": "./static/media/00255.687792a4.jpg"
+  },
+  {
+    "revision": "08dcda8852af97ba3881ab5790df7eb0",
+    "url": "./static/media/00256.08dcda88.jpg"
+  },
+  {
+    "revision": "57d2541a3a959e1c44f3a987624ad55d",
+    "url": "./static/media/00257.57d2541a.jpg"
+  },
+  {
+    "revision": "c7df2ea57b7d43689e2a1644666dc3fb",
+    "url": "./static/media/00258.c7df2ea5.jpg"
+  },
+  {
+    "revision": "2d7dc80afe2c76e0e7c7cc5556ba827f",
+    "url": "./static/media/00259.2d7dc80a.jpg"
+  },
+  {
+    "revision": "32a406636b6bd40fb71ac84912c13635",
+    "url": "./static/media/00260.32a40663.jpg"
+  },
+  {
+    "revision": "f46dded3feb27fbf78bfa5048922893e",
+    "url": "./static/media/00261.f46dded3.jpg"
+  },
+  {
+    "revision": "531251fb87627aec02b9964c34c09495",
+    "url": "./static/media/00262.531251fb.jpg"
+  },
+  {
+    "revision": "57223d3bfa9b8942736d9ed175ffb7d9",
+    "url": "./static/media/00263.57223d3b.jpg"
+  },
+  {
+    "revision": "40bbc476d7109d8a0c9bd863038e423a",
+    "url": "./static/media/00264.40bbc476.jpg"
+  },
+  {
+    "revision": "48bc7b458bbb2c6324427668b6ebbd6b",
+    "url": "./static/media/00265.48bc7b45.jpg"
+  },
+  {
+    "revision": "d9ba56bd176047eeb4d51bbfbdc0996c",
+    "url": "./index.html"
+  }
+];
